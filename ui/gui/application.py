b279d7b71da4d1f768972733a0a6ad8afb603877
Alexander B. Ustinov <ustinov@spbau.ru>
2023-09-21 11:38:33 +0300

    Фикс преждевременной смерти окна

--- ui/gui/application.py
+++ ui/gui/application.py
@@ -2,12 +2,11 @@ from PyQt6.QtWidgets import QApplication
 
 from .widgets import Window
 
+
 class Application(QApplication):
     def __init__(self, *args, **kwargs):
         QApplication.__init__(self, *args, **kwargs)
         window = Window()
         window.resize(200, 100)
         window.show()
-
-
-
+        self.window = window
